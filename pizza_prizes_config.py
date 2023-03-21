84382c943ab0e803ce541fdce3ac8c8cf953ad8a
ndurai2006 <42531049+ndurai2006@users.noreply.github.com>
2023-03-20 23:46:18 -0400

    Add files via upload
    Contains the price details of each Pizza base/size and toppings

---
+++ pizza_prizes_config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 20 23:26:22 2023
+
+@author: Durai
+"""
+PIZZA_PRICES = {
+    "margherita": {"small": 6.99, "medium": 8.99, "large": 11.99},
+    "pepperoni": {"small": 7.99, "medium": 9.99, "large": 13.49},
+    "vegetarian": {"small": 8.99, "medium": 10.99, "large": 14.49},
+}
+
+PIZZA_TOPPINGS = {"mushrooms":0.75, "onions":0.5, "pepperoni":1.0, "sausage":1.0, "none":0}
