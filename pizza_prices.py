4669dd2282ebf073d1fa4b32d881e4dc5b1a9c4f
ndurai2006 <42531049+ndurai2006@users.noreply.github.com>
2023-03-20 23:54:11 -0400

    Add files via upload
    Moved the price information to a separate file pizza_prices_config.py

--- pizza_prices.py
+++ pizza_prices.py
@@ -10,13 +10,8 @@ toppings
 
 Assume the system stores everything in-memory, no storage is required.
 """
-PIZZA_PRICES = {
-    "margherita": {"small": 6.99, "medium": 8.99, "large": 11.99},
-    "pepperoni": {"small": 7.99, "medium": 9.99, "large": 13.49},
-    "vegetarian": {"small": 8.99, "medium": 10.99, "large": 14.49},
-}
 
-PIZZA_TOPPINGS = {"mushrooms":0.75, "onions":0.5, "pepperoni":1.0, "sausage":1.0, "extra cheese":0.5}
+from pizza_prices_config import PIZZA_PRICES, PIZZA_TOPPINGS
 
 def calculate_price(base, size, toppings):
     price = PIZZA_PRICES[base][size]
